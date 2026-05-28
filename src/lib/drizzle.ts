8c2ac0577fbe54fde8562d0853b7a672b4ba61f5
kyooo8 <kyosuke.7876@gmail.com>
2026-05-28 20:10:55 +0900

    tursoを使うようにする

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
-import path from "path";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
 
-const dbPath = path.resolve(process.cwd(), process.env.DB_FILE_NAME!);
-export const db = drizzle(dbPath);
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
